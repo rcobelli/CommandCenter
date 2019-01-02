86fc203a01b73822867a9f7c60ca7afd46dd5458
Ryan Cobelli <ryan.cobelli@rybel-llc.com>
2019-01-01 22:19:31 -0500

    Add auto reload to display

--- display/index.php
+++ display/index.php
@@ -12,7 +12,17 @@ $id = steralizeString($_GET['user']);
 ?>
 <html>
 <head>
-    <title>Command Center | Status Display</title>
+    <title>Command Center | System Status</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="../favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../favicon/favicon-16x16.png">
+    <link rel="manifest" href="../favicon/site.webmanifest">
+    <link rel="mask-icon" href="../favicon/safari-pinned-tab.svg" color="#24273a">
+    <link rel="shortcut icon" href="../favicon/favicon.ico">
+    <meta name="msapplication-TileColor" content="#24273a">
+    <meta name="msapplication-config" content="../favicon/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <meta http-equiv="refresh" content="180" >
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <style>
     .red-dot {
